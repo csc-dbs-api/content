1751186c93f94ca99a9e64c94ce2029b07ef7da6
schakrad <sindhu.chakradari@cscglobal.com>
2026-04-24 11:53:54 -0400

    playbook and tests

--- Packs/CSCDomainSecurity/Integrations/CSCTakedowns/CSCTakedowns_test.py
+++ Packs/CSCDomainSecurity/Integrations/CSCTakedowns/CSCTakedowns_test.py
@@ -1,41 +1,55 @@
-"""Base Integration for Cortex XSOAR - Unit Tests file
-
-Pytest Unit Tests: all funcion names must start with "test_"
-
-More details: https://xsoar.pan.dev/docs/integrations/unit-testing
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-You must add at least a Unit Test function for every XSOAR command
-you are implementing with your integration
-"""
-
-from demisto_sdk.commands.common.handlers import JSON_Handler
-
-import json
+import pytest
+import io
+from CommonServerPython import *
+from CSCTakedowns import Client, controldetectionflowbyeventidandaction_command, fetchthedetectiondataandconverttopdf_command, fetchthephishkitdatawithticketid_command, fetchthescreenshotdatawithticketid_command, fetchtheticketdataandconverttopdf_command, gethtmlsourcecodeforaticket_command, getlistofands_command, getlistoffraudtypes_command, listofworklogsforticketid_command, listtakedownevents_command, listtakedowneventswithfilters_command, performanactiononasingletarget_command, retrieveeventscreenshotwitheventid_command, retrievefilteredlistofmonitoringresultswithinspecifiedtimeframe_command, retrievelistofdetectionswithinspecifiedtimeframe_command, retrievelistofmonitoringresultswithinspecifiedtimeframe_command, retrievephishkitwitheventid_command, startorstopmonitoringforaspecificevent_command, updatetheactionwithticketid_command
+SERVER_URL = 'https://test_url.com'
 
 
 def util_load_json(path):
-    with open(path, encoding="utf-8") as f:
+    with io.open(path, mode='r', encoding='utf-8') as f:
         return json.loads(f.read())
 
 
-# TODO: REMOVE the following dummy unit test function
-def test_baseintegration_dummy():
-    """Tests helloworld-say-hello command function.
+@pytest.fixture()
+def client():
+    return Client(server_url=SERVER_URL, verify=None, proxy=None, headers=None, auth=None)
 
-    Checks the output of the command function with the expected output.
 
-    No mock is needed here because the say_hello_command does not call
-    any external API.
+def test_fetchthephishkitdatawithticketid_command(client, requests_mock):
     """
-    from BaseIntegration import Client, baseintegration_dummy_command
+        When:
+        Given:
+        Then:
+        """
+    args = {'ticketId': '123'}
+    mock_response_fetchthephishkitdatawithticketid_request = util_load_json(
+        './test_data/outputs/fetchthephishkitdatawithticketid_request.json')
+    mock_results = util_load_json(
+        './test_data/outputs/fetchthephishkitdatawithticketid_command.json')
+    requests_mock.post(SERVER_URL, json=mock_response_fetchthephishkitdatawithticketid_request)
+    results = fetchthephishkitdatawithticketid_command(client=client, args=args
+                                                       )
+    assert results.outputs_prefix == '/mnt/c/Users/schakrad/documents/content'
+    assert results.outputs_key_field == ''
+    assert results.outputs == mock_results.get('outputs')
+    assert results.raw_response == mock_response_fetchthephishkitdatawithticketid_request
 
-    client = Client(base_url="some_mock_url", verify=False)
-    args = {"dummy": "this is a dummy response", "dummy2": "a dummy value"}
-    response = baseintegration_dummy_command(client, args)
 
-    assert response.outputs == args
-
-
-# TODO: ADD HERE unit tests for every command
+def test_fetchthescreenshotdatawithticketid_command(client, requests_mock):
+    """
+        When:
+        Given:
+        Then:
+        """
+    args = {'ticketId': '123'}
+    mock_response_fetchthescreenshotdatawithticketid_request = util_load_json(
+        './test_data/outputs/fetchthescreenshotdatawithticketid_request.json')
+    mock_results = util_load_json(
+        './test_data/outputs/fetchthescreenshotdatawithticketid_command.json')
+    requests_mock.post(SERVER_URL, json=mock_response_fetchthescreenshotdatawithticketid_request)
+    results = fetchthescreenshotdatawithticketid_command(client=client,
+                                                         args=args)
+    assert results.outputs_prefix == '/mnt/c/Users/schakrad/documents/content'
+    assert results.outputs_key_field == ''
+    assert results.outputs == mock_results.get('outputs')
+    assert results.raw_response == mock_response_fetchthescreenshotdatawithticketid_request
